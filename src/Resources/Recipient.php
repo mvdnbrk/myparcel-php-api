87732671dcce432f5ded795478346e7f324fe96a
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-13 00:31:10 +0200

    Adds a recipient resource.

---
+++ src/Resources/Recipient.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mvdnbrk\MyParcel\Resources;
+
+class Recipient extends Address
+{
+    /**
+     * @var string
+     */
+    public $company;
+
+    /**
+     * @var string
+     */
+    public $person;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $phone;
+}
---
+++ tests/Unit/Resources/RecipientTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Mvdnbrk\MyParcel\Tests\Unit\Resources;
+
+use PHPUnit\Framework\TestCase;
+use Mvdnbrk\MyParcel\Resources\Recipient;
+
+class RecipientTest extends TestCase
+{
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'company' => 'Test Company B.V.',
+            'person' => 'John Doe',
+            'email' => 'john@example.com',
+            'phone' => '0101111111',
+            'street' => 'Poststraat',
+            'number' => '1',
+            'number_suffix' => 'A',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function creating_a_valid_recipient_resource()
+    {
+        $recipient = new Recipient([
+            'company' => 'Test Company B.V.',
+            'person' => 'John Doe',
+            'email' => 'john@example.com',
+            'phone' => '0101111111',
+            'street' => 'Poststraat',
+            'number' => '1',
+            'number_suffix' => 'A',
+            'postal_code' => '1234AA',
+            'city' => 'Amsterdam',
+            'region' => 'Noord-Holland',
+            'cc' => 'NL',
+        ]);
+
+        $this->assertEquals('Test Company B.V.', $recipient->company);
+        $this->assertEquals('John Doe', $recipient->person);
+        $this->assertEquals('john@example.com', $recipient->email);
+        $this->assertEquals('0101111111', $recipient->phone);
+        $this->assertEquals('1', $recipient->number);
+        $this->assertEquals('A', $recipient->number_suffix);
+        $this->assertEquals('1234AA', $recipient->postal_code);
+        $this->assertEquals('Amsterdam', $recipient->city);
+        $this->assertEquals('Noord-Holland', $recipient->region);
+        $this->assertEquals('NL', $recipient->cc);
+    }
+
+    /** @test */
+    public function country_code_may_be_used_as_an_alias_to_cc()
+    {
+        $recipient = new Recipient([
+            'country_code' => 'NL'
+        ]);
+
+        $this->assertEquals('NL', $recipient->cc);
+    }
+
+    /** @test */
+    public function zipcode_may_be_used_as_an_alias_to_postal_code()
+    {
+        $recipient = new Recipient([
+            'zipcode' => '9999ZZ'
+        ]);
+
+        $this->assertEquals('9999ZZ', $recipient->postal_code);
+    }
+
+    /** @test */
+    public function lower_case_country_code_is_converted_to_uppercase()
+    {
+        $recipient = new Recipient($this->validParams([
+            'cc' => 'nl'
+        ]));
+
+        $this->assertEquals('NL', $recipient->cc);
+    }
+
+    /** @test */
+    public function lower_case_postal_code_is_converted_to_uppercase()
+    {
+        $recipient = new Recipient($this->validParams([
+            'postal_code' => '1234aa'
+        ]));
+
+        $this->assertEquals('1234AA', $recipient->postal_code);
+    }
+}
