9ee16e06bfc3e5de763c0686f1677285766b3a95
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-13 00:59:53 +0200

    Adds Jsonable contract.

--- src/Resources/BaseResource.php
+++ src/Resources/BaseResource.php
@@ -4,9 +4,10 @@ namespace Mvdnbrk\MyParcel\Resources;
 
 use Mvdnbrk\MyParcel\Support\Str;
 use Mvdnbrk\MyParcel\Support\Collection;
+use Mvdnbrk\MyParcel\Contracts\Jsonable;
 use Mvdnbrk\MyParcel\Contracts\Arrayable;
 
-abstract class BaseResource implements Arrayable
+abstract class BaseResource implements Arrayable, Jsonable
 {
     use Concerns\HasAttributes;
 
@@ -37,6 +38,16 @@ abstract class BaseResource implements Arrayable
     }
 
     /**
+     * Convert the recource into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    /**
       * Convert the resource instance to an array.
       *
       * @return array
@@ -44,5 +55,24 @@ abstract class BaseResource implements Arrayable
     public function toArray()
     {
         return $this->attributesToArray();
+    }
+
+    /**
+     * Convert the resource instance to JSON.
+     *
+     * @param  int  $options
+     * @return string
+     *
+     * @throws \Mvdnbrk\MyParcel\\Exceptions\JsonEncodingException
+     */
+    public function toJson($options = 0)
+    {
+        $json = json_encode($this->jsonSerialize(), $options);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw JsonEncodingException::forResource($this, json_last_error_msg());
+        }
+
+        return $json;
     }
 }
