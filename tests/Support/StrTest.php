7abc61e78595beed74a3a3db6059149281f667e8
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-13 13:19:54 +0200

    Adds Support\StrTest.

---
+++ tests/Support/StrTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mvdnbrk\MyParcel\Tests\Support;
+
+use PHPUnit\Framework\TestCase;
+use Mvdnbrk\MyParcel\Support\Str;
+
+class StrTest extends TestCase
+{
+    /** @test */
+    public function testStudly()
+    {
+        $this->assertEquals('MvdnbrkMyParcelPhp', Str::studly('mvdnbrk_my_parcel_php'));
+        $this->assertEquals('MvdnbrkMyparcelPhp', Str::studly('mvdnbrk_myparcel_php'));
+        $this->assertEquals('MvdnbrkMyParcelPhp', Str::studly('mvdnbrk-myParcel-php'));
+        $this->assertEquals('MvdnbrkMyparcelPhp', Str::studly('mvdnbrk  -_-  myparcel   -_-   php   '));
+    }
+
+    /** @test */
+    public function testUpper()
+    {
+        $this->assertEquals('FOO BAR', Str::upper('foo bar'));
+        $this->assertEquals('FOO BAR', Str::upper('foO bAr'));
+    }
+}
