497d682b2653eb74f6c4666eb5327cecbdf63a4d
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-14 22:26:58 +0200

    Adds environmental variables.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidFileException;
+use Dotenv\Exception\InvalidPathException;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp()
+    {
+        try {
+            (new Dotenv('./', '.env'))->load();
+        } catch (InvalidPathException $e) {
 
+        } catch (InvalidFileException $e) {
+            die('The environment file is invalid: '.$e->getMessage());
+        }
+    }
 }
