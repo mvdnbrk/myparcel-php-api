6a7645a473bd6651fc71b2911db305ff044ce040
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-17 22:18:00 +0200

    Adds string functions.

--- tests/Support/StrTest.php
+++ tests/Support/StrTest.php
@@ -8,7 +8,24 @@ use Mvdnbrk\MyParcel\Support\Str;
 class StrTest extends TestCase
 {
     /** @test */
-    public function testStudly()
+    public function lower()
+    {
+        $this->assertEquals('mvdnbrk myparcel', Str::lower('MVDNBRK MYPARCEL'));
+        $this->assertEquals('mvdnbrk myparcel', Str::lower('MvdnBrk MyParcel'));
+    }
+
+    /** @test */
+    public function starts_with()
+    {
+        $this->assertTrue(Str::startsWith('mvdnbrk', 'mvdn'));
+        $this->assertTrue(Str::startsWith('mvdnbrk', 'mvdnbrk'));
+        $this->assertFalse(Str::startsWith('mvdnbrk', 'parcel'));
+        $this->assertFalse(Str::startsWith('mvdnbrk', ''));
+        $this->assertFalse(Str::startsWith('m', ' m'));
+    }
+
+    /** @test */
+    public function studly()
     {
         $this->assertEquals('MvdnbrkMyParcelPhp', Str::studly('mvdnbrk_my_parcel_php'));
         $this->assertEquals('MvdnbrkMyparcelPhp', Str::studly('mvdnbrk_myparcel_php'));
@@ -17,7 +34,7 @@ class StrTest extends TestCase
     }
 
     /** @test */
-    public function testUpper()
+    public function upper()
     {
         $this->assertEquals('FOO BAR', Str::upper('foo bar'));
         $this->assertEquals('FOO BAR', Str::upper('foO bAr'));
