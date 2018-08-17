03c1ec58b1266d58d2e8eab6ad37dba15f0a3979
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-17 19:26:24 +0200

    Adds a to_array test for PickupLocation.

--- tests/Unit/Resources/PickupLocationTest.php
+++ tests/Unit/Resources/PickupLocationTest.php
@@ -30,4 +30,29 @@ class PickupLocationTest extends TestCase
         $pickup->distance = 11500;
         $this->assertEquals('12 km', $pickup->distanceForHumans());
     }
+
+    /** @test */
+    public function to_array()
+    {
+        $pickup = new PickupLocation([
+            'name' => 'Test name',
+            'phone' => '0101111111',
+            'location_code' => 'testcode1234',
+            'opening_hours' => [],
+            'latitude' => 1.11,
+            'longitude' => 2.22,
+            'distance' => 100,
+        ]);
+
+        $array = $pickup->toArray();
+
+        $this->assertInternalType('array', $array);
+        $this->assertEquals('Test name', $array['name']);
+        $this->assertEquals('0101111111', $array['phone']);
+        $this->assertEquals('testcode1234', $array['location_code']);
+        $this->assertEquals([], $array['opening_hours']);
+        $this->assertEquals(1.11, $array['latitude']);
+        $this->assertEquals(2.22, $array['longitude']);
+        $this->assertEquals('100 meter', $array['distance']);
+    }
 }
