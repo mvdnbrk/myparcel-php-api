534c8734055bc3007971bbb1e45cd9a9427a2e18
Mark van den Broek <mvdnbrk@gmail.com>
2018-08-17 16:28:39 +0200

    Adds a Shipment endpoint.

---
+++ src/Endpoints/Shipment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mvdnbrk\MyParcel\Endpoints;
+
+use Mvdnbrk\MyParcel\Resources\Parcel;
+use Mvdnbrk\MyParcel\Resources\Shipment as ShipmentResource;
+
+class Shipment extends BaseEndpoint
+{
+    /**
+     * Create a new shipment for a parcel.
+     *
+     * @param  \Mvdnbrk\MyParcel\Resources\Parcel
+     * @return \Mvdnbrk\MyParcel\Resources\Shipment
+     */
+    public function create(Parcel $parcel)
+    {
+        $httpBody = json_encode([
+            'data' => [
+                'shipments' => [
+                    json_decode($parcel->toJson())
+                ]
+            ],
+        ]);
+
+        $response = $this->performApiCall(
+            'POST',
+            'shipments',
+            $httpBody,
+            ['Content-Type: application/vnd.shipment+json; charset=utf-8']
+        );
+
+        return new ShipmentResource(array_merge([
+            'id' => $response->data->ids[0]->id,
+        ], $parcel->toArray()));
+    }
+}
---
+++ tests/Unit/Endpoints/ShipmentsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Unit\Endpoints;
+
+use Tests\TestCase;
+use Mvdnbrk\MyParcel\Client;
+use Mvdnbrk\MyParcel\Resources\Parcel;
+use Mvdnbrk\MyParcel\Resources\Shipment;
+
+/** @group integration */
+class ShipmentsTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = new Client;
+        $this->client->setApiKey(getenv('API_KEY'));
+    }
+
+    /** @test */
+    public function create_a_new_shipment()
+    {
+        $array = [
+            'carrier' => 1,
+            'reference_identifier' => 'test-123',
+            'recipient' => [
+                'company' => 'Test Company B.V.',
+                'person' => 'John Doe',
+                'email' => 'john@example.com',
+                'phone' => '0101111111',
+                'street' => 'Poststraat',
+                'number' => '1',
+                'number_suffix' => 'A',
+                'postal_code' => '1234AA',
+                'city' => 'Amsterdam',
+                'region' => 'Noord-Holland',
+                'cc' => 'NL',
+            ],
+            'options' => [
+                'label_description' => 'Test label description',
+                'large_format' => false,
+                'only_recipient' => false,
+                'package_type' => 1,
+                'return' => false,
+                'signature' => 1,
+            ],
+        ];
+
+        $parcel = new Parcel($array);
+
+        $shipment = $this->client->shipment->create($parcel);
+
+        $this->assertInstanceOf(Shipment::class, $shipment);
+        $this->assertNotNull($shipment->id);
+    }
+}
