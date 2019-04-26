e7d6dce50f0ed0d96d5b16ee362fba8769110c2d
M. Vugteveen <it-can@users.noreply.github.com>
2019-04-26 16:11:44 +0200

    Adds method to set a label.
    fixes #21

--- src/Endpoints/ShipmentLabels.php
+++ src/Endpoints/ShipmentLabels.php
@@ -8,6 +8,21 @@ use Mvdnbrk\MyParcel\Resources\Shipment;
 class ShipmentLabels extends BaseEndpoint
 {
     /**
+     * @var \Mvdnbrk\MyParcel\Resources\Label
+     */
+    protected $label;
+
+    /**
+     * Boot the ShipmentLabels endpoint.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->label = new Label;
+    }
+
+    /**
      * Get a shipment label by shipment object or id.
      *
      * @param  \Mvdnbrk\MyParcel\Resources\Shipment|int  $value
@@ -22,12 +37,25 @@ class ShipmentLabels extends BaseEndpoint
         $response = $this->performApiCall(
             'GET',
             'shipment_labels/'.$value.$this->buildQueryString(
-                (new Label)->toArray()
+                $this->label->toArray()
             ),
             null,
             ['Accept' => 'application/pdf']
         );
 
         return $response;
+    }
+
+    /**
+     * Set a label.
+     *
+     * @param  \Mvdnbrk\MyParcel\Resources\Label  $label
+     * @return $this
+     */
+    public function setLabel(Label $label)
+    {
+        $this->label = $label;
+
+        return $this;
     }
 }
