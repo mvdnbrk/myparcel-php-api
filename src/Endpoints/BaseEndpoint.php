3ca787657f165ae2ded3de879350c9ad0f4490f1
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-13 00:40:10 +0100

    Revert "Removes unused boot method."
    This reverts commit f55fc177501c29a6b3b112772405ffd92fa9d728.

--- src/Endpoints/BaseEndpoint.php
+++ src/Endpoints/BaseEndpoint.php
@@ -21,6 +21,17 @@ abstract class BaseEndpoint
     public function __construct(Client $client)
     {
         $this->apiClient = $client;
+
+        $this->boot();
+    }
+
+    /**
+     * Boot the endpoint instance.
+     *
+     * @return void
+     */
+    protected function boot()
+    {
     }
 
     /**
