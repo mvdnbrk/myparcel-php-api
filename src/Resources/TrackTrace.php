d1042b9a8453e78721194a5888571f29d72e2ccd
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-12 23:55:28 +0000

    Apply fixes from StyleCI

--- src/Resources/TrackTrace.php
+++ src/Resources/TrackTrace.php
@@ -71,8 +71,8 @@ class TrackTrace extends Trace
     public function setHistoryAttribute($array = [])
     {
         $this->history = collect($array)->map(function ($item) {
-                return collect($item)->all();
-            })
+            return collect($item)->all();
+        })
             ->all();
     }
 }
