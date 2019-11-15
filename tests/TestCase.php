1f159683274fde98dbf7f3fcaeb0587b045535ec
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-15 14:28:01 +0100

    Deprecate Laravel support.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,9 @@ use Dotenv\Dotenv;
 use Dotenv\Exception\InvalidFileException;
 use Dotenv\Exception\InvalidPathException;
 use Mvdnbrk\MyParcel\Client;
-use Mvdnbrk\MyParcel\MyParcelServiceProvider;
-use Orchestra\Testbench\TestCase as Orchestra;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
@@ -24,34 +23,5 @@ abstract class TestCase extends Orchestra
         $this->client = (new Client)->setApiKey(getenv('API_KEY'));
 
         parent::setUp();
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('myparcel.key', getenv('API_KEY'));
-    }
-
-    /**
-     * Get package providers.  At a minimum this is the package being tested, but also
-     * would include packages upon which our package depends, e.g. Cartalyst/Sentry
-     * In a normal app environment these would be added to the 'providers' array in
-     * the config/app.php file.
-     *
-     * @param  \Illuminate\Foundation\Application  $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            MyParcelServiceProvider::class,
-        ];
     }
 }
