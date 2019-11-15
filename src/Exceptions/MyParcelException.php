2c74fc5fc5f30dbe57a784dc2f7cc5a3d400430b
Mark van den Broek <mvdnbrk@gmail.com>
2019-11-15 13:04:47 +0100

    Adds type declaration.
    [ci skip]

--- src/Exceptions/MyParcelException.php
+++ src/Exceptions/MyParcelException.php
@@ -10,7 +10,7 @@ class MyParcelException extends Exception
      * @param  string  $message
      * @param  int  $code
      */
-    public function __construct($message = '', $code = 0)
+    public function __construct(string $message = '', int $code = 0)
     {
         parent::__construct($message, $code);
     }
