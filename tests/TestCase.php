38667ac4f8b54f30dea931ebfae802a70e6a4cfa
Mark van den Broek <mvdnbrk@gmail.com>
2020-06-28 13:36:16 +0200

    Bump version

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         try {
-            (Dotenv::createImmutable(__DIR__.'/..'))->load();
+            (Dotenv::createUnsafeImmutable(__DIR__.'/..'))->load();
         } catch (InvalidPathException $e) {
             //
         } catch (InvalidFileException $e) {
