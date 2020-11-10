06f093b2be454e11f819b65f07d93f21314bef79
Zeno Webdev <info@zeno.site>
2020-11-10 15:55:13 +0100

    Test case updated

--- tests/Unit/Resources/TraceTest.php
+++ tests/Unit/Resources/TraceTest.php
@@ -14,11 +14,15 @@ class TraceTest extends TestCase
             'code' => 'AA1',
             'description' => 'Test description',
             'time' => '2018-08-28 23:00:00',
+			'link_consumer_portal' => 'https://postnl.nl/tracktrace/?B=3SMYPA126329191&P=2182KD&D=NL&T=C&L=NL',
+			'link_tracktrace' => 'https://postnl.nl/tracktrace/?B=3SMYPA126329191&P=2182KD&D=NL&T=C&L=NL',
         ]);
 
         $this->assertEquals('AA1', $tracktrace->code);
         $this->assertEquals('Test description', $tracktrace->description);
         $this->assertEquals('2018-08-28 23:00:00', $tracktrace->time);
+		$this->assertEquals('https://postnl.nl/tracktrace/?B=3SMYPA126329191&P=2182KD&D=NL&T=C&L=NL', $tracktrace->link_consumer_portal);
+		$this->assertEquals('https://postnl.nl/tracktrace/?B=3SMYPA126329191&P=2182KD&D=NL&T=C&L=NL', $tracktrace->link_tracktrace);
     }
 
     /** @test */
