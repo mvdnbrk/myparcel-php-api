8ada7c28bb7573639072359ad441082310ac69eb
Sebastiaan <sebastiaan@avido.nl>
2020-11-27 16:26:13 +0100

    Added Webhook resource

---
+++ src/Resources/Webhook.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mvdnbrk\MyParcel\Resources;
+
+class Webhook extends BaseResource
+{
+    /** @var int */
+    public $id;
+
+    /** @var string */
+    public $hook;
+
+    /** @var string */
+    public $url;
+}
