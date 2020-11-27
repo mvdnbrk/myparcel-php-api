972ea15189ec1e5abb65260beab80ab491431d30
Sebastiaan <sebastiaan@avido.nl>
2020-11-27 17:06:18 +0100

    Updated resource

--- src/Resources/Webhook.php
+++ src/Resources/Webhook.php
@@ -7,6 +7,12 @@ class Webhook extends BaseResource
     /** @var int */
     public $id;
 
+    /** @var int */
+    public $account_id;
+
+    /** @var int */
+    public $shop_id;
+
     /** @var string */
     public $hook;
 
